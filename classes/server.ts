8db8a9d41ad0c77dd4337a8e1ef5f9ca8f6fc4f0
jose alfredo <a00516283@gmail.com>
2018-12-27 20:58:08 -0600

    Primer commit - rest server

---
+++ classes/server.ts
@@ -0,0 +1,18 @@
+//npm install @types/express --save-dev
+import express from "express";
+import {SERVER_PORT} from "../global/environment";
+
+export default class Server {
+	public app: express.Application;
+	public port: number;
+
+	constructor(){
+		this.app = express();
+		this.port = SERVER_PORT	;
+	}
+
+	start(callback: Function){
+		this.app.listen(this.port, callback);
+
+	}
+}
---
+++ index.ts
@@ -0,0 +1,23 @@
+//npm install @types/cors --save-dev
+import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const server = new Server();
+
+//BODYPARSER
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+//CORS
+server.app.use(cors({
+	origin: true,
+	credentials:true
+}))
+
+server.app.use("/", router);
+
+server.start(() => {
+	console.log(`Servidor correindo en el puerto ${server.port}`);
+});
